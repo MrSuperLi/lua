3b2acc0f53ee8c5f03ab52ab6d22f8d173928f56
MrSuperLi <1140926800@qq.com>
2019-03-06 21:41:36 +0800

    【开发】C模块

---
+++ lua_mylib.lua
@@ -0,0 +1,55 @@
+-- 但是对于 自引用 或者 元素之间引用的 这里不能保存。需要变为表达式的方式
+function var_export(_var, returnable)
+    --print(serialize('f', debug.getinfo(2)))
+    local function _var_export(var)
+        local var_type = type(var)
+        if var_type == 'table' then
+            local str = '{'
+            for k,v in pairs(var) do
+                if type(k) == 'string' then
+                    str = str ..'['..string.format("%q", k)..']=' .. _var_export(v) .. ','
+                else
+                    str = str ..'['..tostring(k)..']=' .. _var_export(v) .. ','
+                end
+            end
+            return str .. '}'
+        else
+            return basicSerialize(var)
+        end
+    end
+
+    if returnable then
+        return _var_export(_var)
+    else
+        return print(_var_export(_var))
+    end
+end
+
+function basicSerialize(_var)
+    if type(_var) == 'number' then
+        return tostring(_var)
+    elseif type(_var) == 'function' or type(_var) == 'table' then
+        return 'nil'
+    else
+        return string.format("%q", _var)
+    end
+end
+
+
+
+-- 文件所在的目录加入到 LUA_PATH，这样设定后就可以使用 require 函数 加载C库了。
+
+local f =  package.loadlib("./source.so", "luaopen_mylib")
+
+print(f)
+
+print('mylib', mylib)
+
+mylib = f()
+
+print('f()', mylib)
+
+print('dir', mylib.dir);
+
+
+var_export(mylib.dir('./'))
---
+++ mylib.c
@@ -0,0 +1,53 @@
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+
+// gcc mylib.c -fPIC -llua -shared -o source.so
+
+// https://zhidao.baidu.com/question/551971637903019772.html
+
+// C 库里面的函数
+static int l_dir(lua_State *L)
+{
+    DIR *dir;
+    struct dirent *entry;
+    int i;
+    const char *path = luaL_checkstring(L, 1);
+    /* open directory */
+    dir = opendir(path);
+    if (dir == NULL)
+    {                                       /* error opening the directory? */
+        lua_pushnil(L);                     /*returnniland...*/
+        lua_pushstring(L, strerror(errno)); /* error message */
+        return 2;                           /* number of results */
+    }
+    /* create result table */
+    lua_newtable(L);
+    i = 1;
+    while ((entry = readdir(dir)) != NULL)
+    {
+        lua_pushnumber(L, i++);           /* push key */
+        lua_pushstring(L, entry->d_name); /* push value */
+        lua_settable(L, -3);
+    }
+    closedir(dir);
+    return 1; /* table is already on top */
+}
+
+// 捆绑库里面的函数
+static const struct luaL_Reg mylib[] = {
+    {"dir", l_dir},
+    {NULL, NULL}
+};
+
+int luaopen_mylib(lua_State *L)
+{
+    // 这种方式返回一个函数，运行后返回才会把库返回
+    lua_newtable(L);
+    luaL_setfuncs(L, mylib, 0);
+    return 1;
+}
+
